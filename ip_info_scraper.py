37476f3627ceac933574e35d38504f282c960344
Lucas Noki <mail@lucasnoki.com>
2022-06-16 10:32:05 +0200

    Add argparse, Fix break in case no results are returned, Add option to feed in a list of AS numbers, add outputfile option which can be read in by masscan with the -iL flag

--- ip_info_scraper.py
+++ ip_info_scraper.py
@@ -1,41 +1,72 @@
-#!/usr/bin/env python3
-
-# Easy recon tool that will pull data from ipinfo.io, disply it to stdout and save it to a CSV file.
-
-# Program loop for user input that makes sure the AS number is integers only.
-def input_loop():
-
-    while True:
-        as_number = input("AS number you want to target:")
-        try:
-            verify = int(as_number)
-        except ValueError:
-            # Loop continues until valid intergers are given
-            print("Error! Please use the number only.")
-        else:
-            print("AS",as_number," is valid... Collecting data.")
-            return as_number
-
 import requests
+import argparse
 from bs4 import BeautifulSoup
-import sys
 
-as_number = input_loop()
-url = 'https://ipinfo.io/AS%s'%as_number
-print(f"getting contents from {url}")
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-a", "--as-number", help="AS number you want to target", type=str)
+group.add_argument("-f", "--file", help="Enter a file containing AS numbers")
+parser.add_argument("-o", "--output", help="Name of the output file")
+ARGS = parser.parse_args()
 
-user_agent = {'User-agent': 'Mozilla/5.0'}
-response  = requests.get(url, headers = user_agent)
-html = response.content
+BASE_URL = "https://ipinfo.io/AS"
+USER_AGENT = {'User-agent': 'Mozilla/5.0'}
+RESULTS = []
 
-# Make soup
-soup = BeautifulSoup(html, 'html.parser')
-container = soup.find(id="ipv4-data")
-links = container.findAll('a')
 
-for i,a in enumerate(links):
-    if "Show" not in a.contents[0]:
-        d = ''
-        if (i > 0):
-            d = ','
-        print(f'{d}{a.contents[0]}', end='')
+def process_input() -> list[str]:
+    if ARGS.file:
+        with open(ARGS.file, "r") as handle:
+            urls = handle.readlines()
+        urls = list(map(lambda u: u.strip(), urls))
+    else:
+        urls = [ARGS.as_number]
+    return urls
+
+
+def numbers_only_validator(numbers: list[str]) -> list[str]:
+    result = []
+    for number in numbers:
+        if not str.isdecimal(number):
+            print(f"{number} is invalid!")
+            continue
+        result.append(number)
+    return result
+
+
+def make_request_and_parse_response(as_number: str) -> None:
+    bla = []
+    res = requests.get(f"{BASE_URL}{as_number}", headers=USER_AGENT).content
+    soup = BeautifulSoup(res, "html.parser")
+    container = soup.find(id="ipv4-data")
+
+    # Stops script from breaking in case there aren't any results
+    if not container:
+        return
+    links = container.findAll('a')
+
+    for idx, ip_range in enumerate(links):
+        if "Show" not in ip_range.contents[0]:
+            bla.append(f"{ip_range.contents[0]}")
+    RESULTS.append(",".join(bla))
+
+
+def main():
+    numbers = process_input()
+    validated_numbers = numbers_only_validator(numbers)
+    print(f"Starting requests for {len(validated_numbers)} AS numbers...")
+
+    for number in validated_numbers:
+        make_request_and_parse_response(number)
+    if not RESULTS:
+        print("Sorry better luck next time!")
+        exit()
+
+    [print(x) for x in RESULTS]
+    if ARGS.output:
+        with open(ARGS.output, "w") as handle:
+            [handle.write(x.replace(",", "\n")) for x in RESULTS]
+
+
+if __name__ == '__main__':
+    main()
